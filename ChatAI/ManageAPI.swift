56c4085f0a1d208b1d28f6afe890cb95b7b29eca
Đỗ Minh Quân <minhquan.5322@aol.com>
2022-12-16 19:39:13 +0700

    init source

--- ChatAI/ManageAPI.swift
+++ ChatAI/ManageAPI.swift
@@ -6,3 +6,31 @@
 //
 
 import Foundation
+import OpenAISwift
+
+final class ManageAPI {
+    static let shared = ManageAPI()
+    private var client: OpenAISwift?
+    
+    @frozen enum Constants {
+        static let key = "<Your API key>"
+    }
+    
+    private init() {}
+    
+    public func setup() {
+        self.client = OpenAISwift(authToken: Constants.key)
+    }
+    
+    public func getResponse(input: String, completion: @escaping (Result<String, Error>) -> Void) {
+        client?.sendCompletion(with: input, completionHandler: { result in
+            switch result {
+            case.success(let model):
+                let output = model.choices.first?.text ?? ""
+                completion(.success(output))
+            case.failure(let error):
+                completion(.failure(error))
+            }
+        })
+    }
+}
